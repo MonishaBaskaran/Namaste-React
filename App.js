a1483ec4c6aa7ef39774ce1388966f8071946a06
MonishaBaskaran <monisha610baskar@gmail.com>
2024-07-06 15:05:35 -0400

    episode-01

---
+++ App.js
@@ -0,0 +1,9 @@
+const heading = React.createElement(
+    "h1", 
+    {id: "heading"}, 
+    "Hello world"
+);
+console.log(heading); //this heading is an object which contains type, props, children etc.
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
